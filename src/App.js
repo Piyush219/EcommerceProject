c3063cc4134690974dacb574eba27f47a43eae7b
Piyush219 <piyush66642k@gmail.com>
2021-12-25 13:14:41 +0530

    Making product(store) page protected

--- src/App.js
+++ src/App.js
@@ -4,69 +4,81 @@ import Header from "./Components/Layout/Header/Header";
 import Store from "./Components/Pages/Store";
 import HomePage from "./Components/Pages/HomePage";
 import About from "./Components/Pages/About";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import CartContext from "./Components/StoreContext/CartContext";
 import CartList from "./Components/Cart/CartList";
 import Contact from "./Components/Pages/Contact";
 import Login from "./Components/Pages/Login";
 
 function App() {
-
+  const [isLoggedIn, setIsloggedIn] = useState(false);
 
   const style = {
-    'color': 'white',
-    'backgroundColor': '#383e3e',
-    'borderRadius': '5px',
-    'border': 'none',
-    'height': '34px',
-    'width': '244px',
-    'cursor': 'pointer',
-    'alignItem': 'center',
-    'marginLeft': '2%',
-    'marginTop': '5%'
-  }
-  
-  const[cartItems, setCartItems] = useState(false)
-  const CartItems = () =>{
+    color: "white",
+    backgroundColor: "#383e3e",
+    borderRadius: "5px",
+    border: "none",
+    height: "34px",
+    width: "244px",
+    cursor: "pointer",
+    alignItem: "center",
+    marginLeft: "2%",
+    marginTop: "5%",
+  };
+
+  const [cartItems, setCartItems] = useState(false);
+  const CartItems = () => {
     setCartItems(true);
-  }
-  const cartItemsClose = () =>{
-    setCartItems(false)
-  }
+  };
+  const cartItemsClose = () => {
+    setCartItems(false);
+  };
 
-  const putRequestHandler = async(contact) =>{
-    const response = await fetch("https://react-http-7d042-default-rtdb.firebaseio.com/contact.json", {
-        method: 'POST',
+  const putRequestHandler = async (contact) => {
+    const response = await fetch(
+      "https://react-http-7d042-default-rtdb.firebaseio.com/contact.json",
+      {
+        method: "POST",
         body: JSON.stringify(contact),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-        
-    });
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const data = await response.json();
-    console.log(data)
-
-}
-
+    console.log(data);
+  };
 
   return (
     <CartContext>
-      
       <Header showCartItem={CartItems} />
-      {cartItems && <CartList Close = {cartItemsClose}/>}
+      {cartItems && <CartList Close={cartItemsClose} />}
 
       <Routes>
-        <Route exact path="/" element={<Store/>} />
-        <Route exact path="/about" element={<About/>} />
-        <Route exact path="/homepage" element={<HomePage/>} />
-        <Route exact path="/contact" element={<Contact putRequest = {putRequestHandler}/>} />
-        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<HomePage />} />
+        {isLoggedIn && <Route exact path="/store" element={<Store />} />}
+        <Route exact path="/about" element={<About />} />
+        <Route
+          exact
+          path="/contact"
+          element={<Contact putRequest={putRequestHandler} />}
+        />
+        <Route
+          exact
+          path="/login"
+          element={<Login checkLogin={setIsloggedIn} />}
+        />
+        {!isLoggedIn && <Route path="*" element={<Navigate to="/login"></Navigate>}></Route>}
       </Routes>
 
       <button style={style}>See The Cart</button>
-      
-      <Footer/>
-      
+
+      <Footer />
     </CartContext>
   );
 }
